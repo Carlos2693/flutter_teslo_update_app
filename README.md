0ba0f659be9b91dd3d5d8905102f2f5049423abc
Carlos Buruel <charly.ob.26@gmail.com>
2024-08-21 12:55:30 -0600

    [Impro][CB] Initial repository

---
+++ README.md
@@ -0,0 +1,17 @@
+# flutter_teslo_update_app
+
+Un proyecto de Flutter donde vamos a dar soporte y hacer funcionar para hacer interacción con una imagen montada en Docker.
+
+## Para empezar
+
+Este proyecto es un punto de partida para una aplicación de Flutter con algunas librerias a utilizar. Aqui 
+
+Algunos recursos para comenzar si este es tu primer proyecto de Flutter:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+Para obtener ayuda para comenzar con el desarrollo de Flutter, consulte la
+[documentación en linea](https://docs.flutter.dev/), que ofrece tutoriales,
+ejemplos, orientación sobre el desarrollo móvil y una referencia completa de la API.
+
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main screen'),
+      ),
+      body: const Center(child: Text('Main content'),),
+    );
+  }
+}
